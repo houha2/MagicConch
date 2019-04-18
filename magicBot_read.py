dbd74d97adbd2663ce31fff3160d36b369cae360
houha2 <38798233+houha2@users.noreply.github.com>
2019-04-18 14:50:25 -0500

    Update magicBot_read.py
    This should be the working bot, it replies to any comment that calls it.

--- magicBot_read.py
+++ magicBot_read.py
@@ -1,22 +1,34 @@
 #from urllib.parse import quote_plus
 import praw
+import random
+from praw.models import MoreComments
 
 myUserAgent = 'MagicConch Bot 0.1 (by u/Tris42)'
-
+myClientId = #secret ID 
+myCLientSecret = #secret client per Reddit Api rules
 keyWords = [ 'all hail the magic conch', 'praise the magic conch']
 questionsPhrases = ['will','what should i', 'could i' ]
 replies = ['Maybe some day', 'I don\'t think so', 'no', 'yes', 'Nothing', 'Try asking again']
 reddit = praw.Reddit(user_agent=myUserAgent,
                       client_id=myClientId,
-                      client_secret=myCLientSecret)
+                      client_secret=myCLientSecret,
+                      username='magicConchBot12',
+                      password= #also a secret)
 
 subreddit = reddit.subreddit("bikinibottomTwitter")
-for submission in subreddit.stream.submissions():
+for submission in subreddit.hot(limit=20):
   allComments = submission.comments.list()
   for comment in allComments:
+    if isinstance(comment, MoreComments):
+      continue
     currentComment = comment.body.lower()
     if 'magic conch' in currentComment:
       for phrase in questionsPhrases:
         if phrase in currentComment and '?' in currentComment:
-          #replyTemplate = ''
-          print(comment.body)
+          replyNumber = random.randint(0,5)
+          submission.reply(replies[replyNumber]'-I am a bot, please don\'t hurt me')
+         # print(comment.body)
+         # print(replies[replyNumber],"-I am a bot, please don\'t hurt me")
+         # print('---------------------------------------------\n')
+    else:
+      print("no calls to the conch")
